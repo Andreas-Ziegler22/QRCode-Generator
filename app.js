bffbf0fb92179cc4a9a62b3a50f0a5093ddf95fb
MrBurp <andreass.ziegler@gmail.com>
2023-01-04 23:24:52 +0100

    is not working at all  i need to figure out

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const app = Vue.createApp({
   data: {
     title: "QRCode Generator",
     text: "https://www.google.com.br",
+    qrcode: new QRious({ size: 300 }),
+  },
+  computed: {
+    newQRCode() {
+      this.qrcode.value = this.text;
+      return this.qrcode.toDataURL();
+    },
+  },
+  methods: {
+    onData(event) {
+      this.text = event.target.value;
+    },
   },
 });
 app.mount("#app");
